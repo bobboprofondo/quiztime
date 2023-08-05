8cb13a97c4b3ad94cc28f8cc5f18764021fba6ca
Bob Forbes <bobboprofondo@gmail.com>
2023-08-05 15:00:17 +0100

    saves to firestore

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,6 +2,7 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { firebaseApp } from "../../firebase/quizfire";
+import { getFirestore, serverTimestamp, collection, addDoc} from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 
@@ -18,13 +19,25 @@ export default function Page() {
     auth.signOut();
   }
 
+  const testSave = () => {
+    const db = getFirestore(firebaseApp);
+    const docco = {
+      msg: 'Bramp',
+      timestamp: serverTimestamp(),
+      uid: user.uid
+    }
+    console.log(docco)
+    addDoc(collection(db,'chats'),docco);
+    // setDoc(doc(db, 'chats', '22vSXIhnIIJzygzCB5nL'), docco);
+  }
+
   const [user, loading] = useAuthState(auth);
 
   return (
     <div>
       <header></header>
-      <section>
-        {user ? <ChatRoom user={user} signOut={signOut} /> : <SignIn signIn={signIn} />}
+      <section className="h-full w-full">
+        {user ? <ChatRoom user={user} signOut={signOut} testSave={testSave} /> : <SignIn signIn={signIn} />}
       </section>
     </div>
   );
@@ -32,9 +45,12 @@ export default function Page() {
 
 function ChatRoom(props) {
   return (
-    <div>
-      <div>Welcome to Quizzy time {props?.user?.displayName || 'Unknown User'}</div>
-      <div><SignOut signOut={props.signOut} /></div>
+    <div className="h-full w-full flex flex-col place-items-center">
+      <div className="flex-auto place-self-center">Welcome to Quizzy time {props?.user?.displayName || 'Unknown User'}</div>
+      <div className="flex-auto"><SignOut signOut={props.signOut} /></div>
+      <div>
+        <button onClick={props.testSave}>Click here!</button>
+      </div>
     </div>
   );
 }
